5cb194f4b560b8bf2147098112dd4cad530973a9
isaiah.v@comcast.net <isaiah.v@comcast.net@56e07a14-6a4f-067a-b4ab-40942c67caf6>
2012-04-20 03:41:48 +0000

    edited echo example
    git-svn-id: http://java-gearman-service.googlecode.com/svn/trunk@131 56e07a14-6a4f-067a-b4ab-40942c67caf6

--- examples/echo/EchoClientAsynchronous.java
+++ examples/echo/EchoClientAsynchronous.java
@@ -4,9 +4,10 @@ import org.gearman.Gearman;
 import org.gearman.GearmanClient;
 import org.gearman.GearmanJobEvent;
 import org.gearman.GearmanJobEventCallback;
+import org.gearman.GearmanJoin;
 import org.gearman.GearmanServer;
 
-public class EchoClientAsynchronous {
+public class EchoClientAsynchronous implements GearmanJobEventCallback<String> {
 	
 	public static void main(String...args) throws InterruptedException {
 		
@@ -20,24 +21,24 @@ public class EchoClientAsynchronous {
 		
 		client.addServer(server);
 		
-		client.submitJob(EchoWorker.ECHO_FUNCTION_NAME, ("Hello World").getBytes(), EchoWorker.ECHO_FUNCTION_NAME, new GearmanJobEventCallback<String>() {
-			@Override
-			public void onEvent(String attachment, GearmanJobEvent event) {
-				
-				// Poll the next job event (blocking operation)
-				
-				switch(event.getEventType()) {
-				case GEARMAN_JOB_SUCCESS:	// Job completed successfully
-					System.out.println(new String(event.getData()));
-					break;
-				case GEARMAN_SUBMIT_FAIL:	// The job submit operation failed
-				case GEARMAN_JOB_FAIL:		// The job's execution failed
-					System.err.println(event.getEventType() + ": " + new String(event.getData()));
-				case GEARMAN_EOF:
-					gearman.shutdown();
-				}
-				
-			}
-		});
+		GearmanJoin<String> join = client.submitJob(EchoWorker.ECHO_FUNCTION_NAME, ("Hello World").getBytes(), EchoWorker.ECHO_FUNCTION_NAME, new EchoClientAsynchronous());
+		
+		join.join();
+		
+		gearman.shutdown();
+	}
+
+	@Override
+	public void onEvent(String functionName, GearmanJobEvent event) {
+		
+		switch(event.getEventType()) {
+		case GEARMAN_JOB_SUCCESS:	// Job completed successfully
+			System.out.println(new String(event.getData()));
+			break;
+		case GEARMAN_SUBMIT_FAIL:	// The job submit operation failed
+		case GEARMAN_JOB_FAIL:		// The job's execution failed
+			System.err.println(event.getEventType() + ": " + new String(event.getData()));
+		}
+		
 	}
 }
