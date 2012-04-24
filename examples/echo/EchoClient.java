e7536120719f56fe76ce577d424fd4ca85715a90
isaiah.v@comcast.net <isaiah.v@comcast.net@56e07a14-6a4f-067a-b4ab-40942c67caf6>
2012-04-24 03:17:55 +0000

    git-svn-id: http://java-gearman-service.googlecode.com/svn/trunk@142 56e07a14-6a4f-067a-b4ab-40942c67caf6

--- examples/echo/EchoClient.java
+++ examples/echo/EchoClient.java
@@ -6,39 +6,91 @@ import org.gearman.GearmanJobEvent;
 import org.gearman.GearmanJobReturn;
 import org.gearman.GearmanServer;
 
+/**
+ * The echo client submits an "echo" job to a job server and prints the final
+ * result. It's the "Hello World" of the java-geraman-service
+ * 
+ * The echo example illustrates how send a single job and get the result
+ */
 public class EchoClient {
-	
-	public static void main(String...args) throws InterruptedException {
-		
-		// Create a new gearman instance
+
+	public static void main(String... args) throws InterruptedException {
+
+		/*
+		 * Create a Gearman instance
+		 */
 		Gearman gearman = Gearman.createGearman();
-		
-		// Create the job server object. This call creates an object represents a remote job server
-		GearmanServer server = gearman.createGearmanServer(EchoWorker.ECHO_HOST, EchoWorker.ECHO_PORT);
-		
+
+		/*
+		 * Create a new gearman client.
+		 * 
+		 * The client is used to submit requests the job server.
+		 */
 		GearmanClient client = gearman.createGearmanClient();
-		
+
+		/*
+		 * Create the job server object. This call creates an object represents
+		 * a remote job server.
+		 * 
+		 * Parameter 1: the host address of the job server.
+		 * Parameter 2: the port number the job server is listening on.
+		 * 
+		 * A job server receives jobs from clients and distributes them to
+		 * registered workers.
+		 */
+		GearmanServer server = gearman.createGearmanServer(
+				EchoWorker.ECHO_HOST, EchoWorker.ECHO_PORT);
+
+		/*
+		 * Tell the client that it may connect to this server when submitting
+		 * jobs.
+		 */
 		client.addServer(server);
-		
-		GearmanJobReturn jobReturn = client.submitJob(EchoWorker.ECHO_FUNCTION_NAME, ("Hello World").getBytes());
-		
-		while(!jobReturn.isEOF()) {
-			
+
+		/*
+		 * Submit a job to a job server.
+		 * 
+		 * Parameter 1: the gearman function name
+		 * Parameter 2: the data passed to the server and worker
+		 * 
+		 * The GearmanJobReturn is used to poll the job's result
+		 */
+		GearmanJobReturn jobReturn = client.submitJob(
+				EchoWorker.ECHO_FUNCTION_NAME, ("Hello World").getBytes());
+
+		/*
+		 * Iterate through the job events until we hit the end-of-file
+		 */
+		while (!jobReturn.isEOF()) {
+
 			// Poll the next job event (blocking operation)
 			GearmanJobEvent event = jobReturn.poll();
-			
-			switch(event.getEventType()) {
-			case GEARMAN_JOB_SUCCESS:	// Job completed successfully
+
+			switch (event.getEventType()) {
+
+			// success
+			case GEARMAN_JOB_SUCCESS: // Job completed successfully
+				// print the result
 				System.out.println(new String(event.getData()));
 				break;
-			case GEARMAN_SUBMIT_FAIL:	// The job submit operation failed
-			case GEARMAN_JOB_FAIL:		// The job's execution failed
-				System.err.println(event.getEventType() + ": " + new String(event.getData()));
+
+			// failure
+			case GEARMAN_SUBMIT_FAIL: // The job submit operation failed
+			case GEARMAN_JOB_FAIL: // The job's execution failed
+				System.err.println(event.getEventType() + ": "
+						+ new String(event.getData()));
+
 			}
-			
+
 		}
-		
-		// shutdown the gearman service
+
+		/*
+		 * Close the gearman service after it's no longer needed. (closes all
+		 * sub-services, such as the client)
+		 * 
+		 * It's suggested that you reuse Gearman and GearmanClient instances
+		 * rather recreating and closing new ones between submissions
+		 */
 		gearman.shutdown();
 	}
 }
--- examples/echo/EchoWorker.java
+++ examples/echo/EchoWorker.java
@@ -1,7 +1,5 @@
 package echo;
 
-import java.io.IOException;
-
 import org.gearman.Gearman;
 import org.gearman.GearmanFunction;
 import org.gearman.GearmanFunctionCallback;
@@ -9,57 +7,67 @@ import org.gearman.GearmanServer;
 import org.gearman.GearmanWorker;
 
 /**
+ * The echo worker polls jobs from a job server and execute the echo function.
  * 
- * @author isaiah
- *
+ * The echo worker illustrates how to setup a basic worker
  */
 public class EchoWorker implements GearmanFunction {
-	
+
 	/** The echo function name */
 	public static final String ECHO_FUNCTION_NAME = "echo";
-	
+
 	/** The host address of the job server */
 	public static final String ECHO_HOST = "localhost";
-	
+
 	/** The port number the job server is listening on */
 	public static final int ECHO_PORT = 4730;
 
-	/**
-	 * Creates an echo worker. A worker receives jobs from the job server distributes them
-	 * to its registered functions.
-	 * 
-	 * @param args
-	 * 		N/A
-	 * @throws IOException
-	 * 		If an IO exception occurs during execution
-	 */
 	public static void main(String... args) {
-		
-		// Create a new gearman instance
+
+		/*
+		 * Create a Gearman instance
+		 */
 		Gearman gearman = Gearman.createGearman();
-		
-		// Create the job server object. This call creates an object represents a remote job server
-		GearmanServer server = gearman.createGearmanServer(EchoWorker.ECHO_HOST, EchoWorker.ECHO_PORT);
-		
-		// Create a gearman worker
+
+		/*
+		 * Create the job server object. This call creates an object represents
+		 * a remote job server.
+		 * 
+		 * Parameter 1: the host address of the job server.
+		 * Parameter 2: the port number the job server is listening on.
+		 * 
+		 * A job server receives jobs from clients and distributes them to
+		 * registered workers.
+		 */
+		GearmanServer server = gearman.createGearmanServer(
+				EchoWorker.ECHO_HOST, EchoWorker.ECHO_PORT);
+
+		/*
+		 * Create a gearman worker. The worker poll jobs from the server and
+		 * executes the corresponding GearmanFunction
+		 */
 		GearmanWorker worker = gearman.createGearmanWorker();
-		
-		// Tell the worker how to perform the echo function
+
+		/*
+		 *  Tell the worker how to perform the echo function
+		 */
 		worker.addFunction(EchoWorker.ECHO_FUNCTION_NAME, new EchoWorker());
-		
-		
-		// Tell the worker that it may communicate with the given job server
+
+		/*
+		 *  Tell the worker that it may communicate with the this job server
+		 */
 		worker.addServer(server);
 	}
-	
+
 	@Override
-	public byte[] work(String function, byte[] data, GearmanFunctionCallback callback) throws Exception {
-		
+	public byte[] work(String function, byte[] data,
+			GearmanFunctionCallback callback) throws Exception {
+
 		/*
-		 * The work method performs the gearman function. In this case, the echo function simply returns
-		 * the data it received
+		 * The work method performs the gearman function. In this case, the echo
+		 * function simply returns the data it received
 		 */
-		
+
 		return data;
 	}
 
